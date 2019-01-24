ef24a3533336f6536495c934a59b895a4a597011
marczak.mat@gmail.com <marczak.mat@gmail.com>
2019-01-24 21:22:55 +0100

    class classOfSchool

---
+++ src/main/java/pl/school/model/ClassOfSchool.java
@@ -0,0 +1,29 @@
+package pl.school.model;
+
+import lombok.Data;
+import pl.school.SchoolStaff.Model.Teacher;
+
+import java.util.ArrayList;
+
+@Data
+public class ClassOfSchool {
+
+    int numberOfStudents;
+    int levelOfEducation; // like 1, 2 etc
+    int id;
+    String name; // 1a, 1b
+    Teacher teacher; //as a tutor
+    List <Student> studentList = new ArrayList<>();
+
+
+    @Override
+    public String toString() {
+        return "ClassOfSchool{" +
+                "numberOfStudents=" + numberOfStudents +
+                ", levelOfEducation=" + levelOfEducation +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", teacher=" + teacher +
+                '}';
+    }
+}
---
+++ src/main/java/pl/school/service/ServiceClassOfSchool.java
@@ -0,0 +1,15 @@
+package pl.school.service;
+
+import pl.school.model.ClassOfSchool;
+
+public interface ServiceClassOfSchool {
+
+    public ClassOfSchool create ();
+
+    public ClassOfSchool read();
+
+    public void edit ();
+
+    public void delete();
+
+}
